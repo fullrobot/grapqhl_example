ae166470e8ec230498fe58a389ea24041571227d
Adam Gleason <adam.gleason@sparktx.com>
2023-05-02 12:11:31 -0400

    graphql_example

---
+++ graphql_test/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from graphql_test.users.models import User
+from graphql_test.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
